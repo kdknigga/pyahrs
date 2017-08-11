f49f415712f2da359716389f37b1027735517cf8
Kris Knigga <kris@knigga.com>
2017-08-10 21:57:16 -0500

    Bug fixes, made orientation input more modular, added pitch labels

--- hud.py
+++ hud.py
@@ -3,7 +3,55 @@
 import os, sys, random
 import pygame
 
+class Vehicle(object):
+    def __init__(self, data_source="random"):
+        self.data_source = data_source
+        self.roll = 0.0
+        self.pitch = 0.0
+        self.yaw = 0.0
+        self.alt = 0
+        self.airspeed = 0
+
+    def get_orientation(self):
+        self.update_orientation()
+        return {'roll':     self.roll,
+                'pitch':    self.pitch,
+                'yaw':      self.yaw,
+                'alt':      self.alt,
+                'airspeed': self.airspeed}
+
+    def set_orientation(self, roll=None, pitch=None, yaw=None, alt=None, airspeed=None):
+        if roll != None:
+            self.roll = roll
+
+        if pitch != None:
+            self.pitch = pitch
+
+        if yaw != None:
+            self.yaw = yaw
+
+        if alt != None:
+            self.alt = alt
+
+        if airspeed != None:
+            self.airspeed = airspeed
+
+    def update_orientation(self):
+        if self.data_source == "random":
+            r = self.roll + random.normalvariate(0, 0.5)
+            p = self.pitch + random.normalvariate(0, 0.5)
+            self.set_orientation(roll=r, pitch=p)
+
+        elif self.data_source == "manual":
+            r = float(raw_input('Roll? '))
+            p = float(raw_input('Pitch? '))
+            self.set_orientation(roll=r, pitch=p)
+
+        else:
+            raise ValueError
+
 def display_init():
+    pygame.init()
     disp_no = os.getenv('DISPLAY')
     if disp_no:
     #if False:
@@ -34,7 +82,7 @@ def display_init():
 
     return screen, size
 
-def get_line_coords (pitch, screen_width, screen_height, ahrs_center):
+def get_line_coords(pitch, screen_width, screen_height, ahrs_center):
     if pitch == 0:
         length = screen_width*.6
     elif (pitch%10) == 0:
@@ -48,18 +96,20 @@ def get_line_coords (pitch, screen_width, screen_height, ahrs_center):
 
     start_x = ahrs_center_x - (length / 2)
     end_x = ahrs_center_x + (length / 2)
-    y = (px_per_deg_y * pitch) + ahrs_center_y
+    y = (px_per_deg_y * -pitch) + ahrs_center_y
 
     return [[start_x, y], [end_x, y]]
 
 def main():
     screen, screen_size = display_init()
     width, height = screen_size
+    pygame.mouse.set_visible(False)
 
-    WHITE =   (255, 255, 255)
+    WHITE = (255, 255, 255)
 
-    roll = 0.0
-    pitch = 0.0
+    font = pygame.font.SysFont(None, int(height/20))
+
+    v = Vehicle(data_source="random")
 
     ahrs_bg = pygame.Surface((width*2, height*2))
     ahrs_bg_width = ahrs_bg.get_width()
@@ -67,7 +117,15 @@ def main():
     ahrs_bg_center = (ahrs_bg_width/2, ahrs_bg_height/2)
 
     for l in range(-60, 60, 5):
-        pygame.draw.lines(ahrs_bg, WHITE, False, get_line_coords(l, width, height, ahrs_bg_center), 2)
+        line_coords = get_line_coords(l, width, height, ahrs_bg_center)
+        pygame.draw.lines(ahrs_bg, WHITE, False, line_coords, 2)
+
+        if l != 0 and l%10 == 0:
+            text = font.render(str(l), False, WHITE)
+            text_width, text_height = text.get_size()
+            left = int(line_coords[0][0]) - (text_width + int(width/100))
+            top = int(line_coords[0][1]) - text_height / 2
+            ahrs_bg.blit(text, (left, top))
 
     done = False
     clock = pygame.time.Clock()
@@ -77,6 +135,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+
+        o = v.get_orientation()
+        roll = o['roll']
+        pitch = o['pitch']
 
         print "Roll:  {:.1f}".format(roll)
         print "Pitch: {:.1f}".format(pitch)
@@ -93,10 +155,9 @@ def main():
         top_left = (-(ahrs.get_width() - width)/2, -(ahrs.get_height() - height)/2)
         screen.blit(ahrs, top_left)
 
+        pygame.draw.lines(screen, WHITE, False, [[0, height/2], [10, height/2]], 2)
+        pygame.draw.lines(screen, WHITE, False, [[width-10, height/2], [width, height/2]], 2)
         pygame.display.flip()
-
-        roll = roll + random.normalvariate(0, 0.5)
-        pitch = pitch + random.normalvariate(0, 0.5)
 
     pygame.quit()
 
